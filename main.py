9ed770c68e8c68864681f98bf95aec1a29b260c7
Orion Railean <orion.railean@gmail.com>
2023-10-04 11:29:29 +1100

    add logging so we know where the problem is

--- main.py
+++ main.py
@@ -3,36 +3,32 @@ from discord.ext import commands
 from PIL import Image
 from io import BytesIO
 
-intents = discord.Intents.default()
-intents.message_content = True
+intents = discord.Intents.all()
 
 CAUGHT_PATTERN = r'<@!*\d+> You caught \*\*(.+)!\*\* \(`#(.+)`\)[\s\S]*'
 
 activity = discord.Streaming(name="Ballsdex Answers", url="https://twitch.tv/SorkoPiko")
-client = commands.Bot(command_prefix='bs!', intents=intents, activity=activity)
+client = commands.Bot(command_prefix='?', intents=intents, activity=activity)
 
-class SetEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, set):
-            return list(obj)
-        return super().default(obj)
+class CustomJSONEncoder(json.JSONEncoder):
+	def default(self, obj):
+		if isinstance(obj, set):
+			return {'__set__': list(obj)}
+		return super().default(obj)
 
-class SetDecoder(json.JSONDecoder):
-    def __init__(self, *args, **kwargs):
-        super().__init__(object_hook=self.set_hook, *args, **kwargs)
-        
-    def set_hook(self, dct):
-        if "__set__" in dct:
-            return set(dct["__set__"])
-        return dct
+# Custom JSON decoder with support for sets
+def custom_decoder(dct):
+	if '__set__' in dct:
+		return set(dct['__set__'])
+	return dct
 
-def getHashes() -> dict[imagehash.ImageHash, dict['names': set[str]]]:
+def getHashes() -> dict[imagehash.ImageHash, dict]:
 	with open('db.json', 'r') as f:
-		return json.loads(f.read(), cls=SetDecoder)
+		return json.loads(f.read(), object_hook=custom_decoder)
 
 def saveHashes(hashes: dict[imagehash.ImageHash, dict]) -> None:
 	with open('db.json', 'w') as f:
-		f.write(json.dumps(hashes, cls=SetEncoder))
+		f.write(json.dumps(hashes, cls=CustomJSONEncoder))
 
 def hashImageURL(url: str) -> imagehash.ImageHash:
 	return imagehash.average_hash(Image.open(BytesIO(requests.get(url).content)))
@@ -43,16 +39,6 @@ def identify(imageHash: imagehash.ImageHash):
 		return hashes[imageHash]['names']
 	else:
 		return None
-
-@client.command(name='add', alias=['a'])
-@commands.is_owner()
-async def add(url, *names):
-	imageHash = hashImageURL(url)
-	hashes = getHashes()
-	if imageHash in hashes:
-		hashes[imageHash]['names'].update(names)
-	hashes[imageHash] = {'status': 'identified', 'names': names}
-	saveHashes(hashes)
 
 @client.event
 async def on_ready():
@@ -76,19 +62,35 @@ async def on_message(message: discord.Message):
 				# hashes[imageHash] = {'status': 'unidentified', 'message': message.id}
 		else:
 			caughtMatch = re.match(CAUGHT_PATTERN, message.content)
+			print(message.content)
+			print(message.flags)
+			print(caughtMatch)
 			if caughtMatch:
-				hashes = getHashes()
+				print(caughtMatch.group(1))
+				print('match!!')
 				originalMessage = await message.channel.fetch_message(message.reference.message_id)
 				imageHash = str(imagehash.average_hash(Image.open(BytesIO(requests.get(originalMessage.attachments[0].url).content))))
+				hashes = getHashes()
 
 				if imageHash in hashes:
-					if caughtMatch.group(1) not in hashes[imageHash]['names']:
-						hashes[imageHash]['names'].append(caughtMatch.group(1))
-						await message.add_reaction('✅')
+					hashes[imageHash]['names'].add(caughtMatch.group(1))
+					await message.add_reaction('✅')
 				else:
 					hashes[imageHash] = {'status': 'identified', 'names': {caughtMatch.group(1)}}
 					await message.add_reaction('✅')
 				
 				saveHashes(hashes)
+
+@client.tree.command()
+async def add(interaction: discord.Interaction, image: discord.Attachment, names: str):
+	if interaction.user.id == 609544328737456149:
+		imageHash = str(hashImageURL(image.url))
+		hashes = getHashes()
+		if imageHash in hashes:
+			hashes[imageHash]['names'].update(names.split(','))
+		else:
+			hashes[imageHash] = {'status': 'identified', 'names': set(names.split(','))}
+		saveHashes(hashes)
+		await interaction.response.send_message('Added!', ephemeral=True)
 
 client.run(os.getenv('TOKEN'))
