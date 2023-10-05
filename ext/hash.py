0d42ee01864f8330043580f1b2fc76e64595b20a
Orion Railean <orion.railean@gmail.com>
2023-10-06 08:49:25 +1100

    fix var naming

--- ext/hash.py
+++ ext/hash.py
@@ -26,15 +26,15 @@ class Hash(commands.GroupCog):
 
 	@app_commands.command(name='compare', description='Compare two images\' hashes.')
 	@app_commands.describe(image1='The first image to compare.', image2='The second image to compare.')
-	async def hash_compare(self, interaction: discord.Interaction, image: discord.Attachment, image2: discord.Attachment):
-		if not image.content_type.startswith('image') or not image2.content_type.startswith('image'):
+	async def hash_compare(self, interaction: discord.Interaction, image1: discord.Attachment, image2: discord.Attachment):
+		if not image1.content_type.startswith('image') or not image2.content_type.startswith('image'):
 			await interaction.response.send_message('Please attach images to the command!', ephemeral=True)
 			return
-		imageHash = str(hashImageURL(image.url))
+		imageHash = str(hashImageURL(image1.url))
 		image2Hash = str(hashImageURL(image2.url))
-		embed = discord.Embed(title=f'{image.filename}', colour=discord.Colour(getAverageColour(urlToImage(image))))
+		embed = discord.Embed(title=f'{image1.filename}', colour=discord.Colour(getAverageColour(urlToImage(image1))))
 		embed.set_image(url=image2.url)
-		embed.set_thumbnail(url=image.url)
+		embed.set_thumbnail(url=image1.url)
 		embed.set_author(name=f'{interaction.user.display_name}', icon_url=interaction.user.display_avatar.url)
 		embed.add_field(name='Image 1 Hash', value=imageHash, inline=True)
 		embed.add_field(name='Image 2 Hash', value=image2Hash, inline=True)
