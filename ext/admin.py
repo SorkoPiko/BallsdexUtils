1d4afd96ba612d906999c536b2f9dd3657d9090c
Orion Railean <orion.railean@gmail.com>
2023-10-07 11:28:33 +1100

    add admin commands

---
+++ ext/admin.py
@@ -0,0 +1,21 @@
+import discord
+from discord.ext import commands
+from helper	import *
+from main import BallsdexUtils
+from discord import app_commands
+
+VERSION = "0.1.0"
+
+class Admin(commands.Cog):
+	def __init__(self, bot: BallsdexUtils):
+		self.bot = bot
+		self.bot.logger.info(f'Loaded Cog:{self.__cog_name__} v{VERSION}')
+
+	@commands.command(name='reload', description='Reloads a cog.')
+	@commands.is_owner()
+	async def admin_reload(self, ctx: commands.Context, cog: str):
+		try:
+			self.bot.reload_extension(f'ext.{cog}')
+		except commands.ExtensionNotLoaded:
+			self.bot.load_extension(f'ext.{cog}')
+		await ctx.send(f'Reloaded {cog}!')
